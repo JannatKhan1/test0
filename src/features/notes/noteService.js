3dfa374345822cbc35b210d9715c009267eb1374
Jannat Khan <89721610+JannatKhan1@users.noreply.github.com>
2023-11-16 12:53:41 +0530

    Update noteService.js

--- src/features/notes/noteService.js
+++ src/features/notes/noteService.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API_URL = 'https://bs-ho-ja.onrender.com/api/data/admin-resolve/'
+const API_URL = 'https://ticket-support-system-backend-6.onrender.com/api/data/admin-resolve/'
 
 // Get ticket notes
 const getNotes = async (ticketId) => {
